e75ea7f4e22d0abc4b271b9733f3b706f67d96f9
YanaBaloshenko <ybalyoshenko1405@gmail.com>
2024-11-11 09:33:44 +0100

    test

---
+++ backend/book/Ch3/ch3_01.py
@@ -0,0 +1,7 @@
+from typing import Union
+
+def print_name_x_times(name: str, times: int) -> None:
+    for _ in range(times):
+        print(name)
+
+x: str | int
---
+++ backend/book/Ch3/ch3_03.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+def format_datetime(dt: datetime) -> str:
+     return dt.strftime("%Y-%m-%d %H:%M:%S")
+now = datetime.now()
+print(format_datetime(now))
